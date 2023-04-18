cb6313a9e5c4950bdf127c0619ceb9e17eb04741
PrakharPandeyyy <8.prakharpandey@gmail.com>
2023-04-18 17:00:13 +0530

    added login page

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-
+const path = require('path')
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(__dirname + "/public"));
 app.use('/img', express.static(__dirname + "/img"));
